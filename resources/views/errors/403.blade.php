71ec2776879aed17fbc08e836b70330603f437a5
Vihanga Rashan Silva <vihangarashansilva@gmail.com>
2024-07-20 12:24:37 +0530

    Initial Commit

---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,5 @@
+<x-app-layout>
+    <div class="w-full h-full flex items-center justify-center">
+        <h1>Forbidden</h1>
+    </div>
+</x-app-layout>
---
+++ resources/views/note/create.blade.php
@@ -0,0 +1,15 @@
+<x-app-layout>
+   <div class="note-container single-note">
+    <h1>Create new Note</h1>
+
+    <form action={{ route('note.store') }} method="post" class="note">
+        @csrf
+        <textarea name="note" rows="10" class="note-body" placeholder="Enter your note here"></textarea>
+
+        <div class="note-buttons">
+            <a href={{ route('note.index') }} class="note-cancel-button">Cancel</a>
+            <button type="submit" class="note-submit-button">Submit</button>
+        </div>
+    </form>
+   </div>
+</x-app-layout>
